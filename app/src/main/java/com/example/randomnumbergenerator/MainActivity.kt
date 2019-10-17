ac9629bac597f6a501cd8deb7a0eb3c64160488f
Saul <saulc@users.noreply.github.com>
2019-10-17 03:12:46 -0700

    gui
    base ui only.

--- app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomnumbergenerator/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.randomnumbergenerator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val n1 = findViewById<EditText>(R.id.num1)
+
+
     }
 }
